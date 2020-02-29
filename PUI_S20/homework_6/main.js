bdb15c258383c66c23b86b3238966d3e102089cb
Yumi S <ysato@andrew.cmu.edu>
2020-02-29 00:56:56 -0500

    implemented quantity and glaze js functions

--- PUI_S20/homework_6/main.js
+++ PUI_S20/homework_6/main.js
@@ -1,1 +1,74 @@
-asdf
+function addOriginalRoll(item) {
+    var cart = document.getElementById("cart");
+    var newItem = document.createElement("li")
+    if ("empty" in cart.classList) {
+        cart.className = "cart";
+    }
+    cart.appendChild(item);
+}
+
+function deleteRoll(item) {
+    item.parentNode.removeChild(item);
+}
+
+function pickQuantity(item) {
+    var one = document.getElementById("one");
+    var three = document.getElementById("three");
+    var six = document.getElementById("six");
+    var twelve = document.getElementById("twelve");
+    if (item.id == "one"){
+        one.style.backgroundColor = "#fce5cdff";
+        three.style.backgroundColor = "white";
+        six.style.backgroundColor = "white";
+        twelve.style.backgroundColor = "white";
+    }
+    else if (item.id == "three") {
+        one.style.backgroundColor = "white";
+        three.style.backgroundColor = "#fce5cdff";
+        six.style.backgroundColor = "white";
+        twelve.style.backgroundColor = "white";
+    }
+    else if (item.id == "six") {
+        one.style.backgroundColor = "white";
+        three.style.backgroundColor = "white";
+        six.style.backgroundColor = "#fce5cdff";
+        twelve.style.backgroundColor = "white";
+    }
+    else {
+        one.style.backgroundColor = "white";
+        three.style.backgroundColor = "white";
+        six.style.backgroundColor = "white";
+        twelve.style.backgroundColor = "#fce5cdff";     
+    }
+}
+
+function pickGlaze(item) {
+    var none = document.getElementById("none");
+    var sugar = document.getElementById("sugar");
+    var vanilla = document.getElementById("vanilla");
+    var chocolate = document.getElementById("chocolate");
+    if (item.id == "none") {
+        none.style.backgroundColor = "#fce5cdff";
+        sugar.style.backgroundColor = "white";
+        vanilla.style.backgroundColor = "white";
+        chocolate.style.backgroundColor = "white";
+    }
+    else if (item.id == "sugar") {
+        none.style.backgroundColor = "white";
+        sugar.style.backgroundColor = "#fce5cdff";
+        vanilla.style.backgroundColor = "white";
+        chocolate.style.backgroundColor = "white";
+    }
+    else if (item.id == "vanilla") {
+        none.style.backgroundColor = "white";
+        sugar.style.backgroundColor = "white";
+        vanilla.style.backgroundColor = "#fce5cdff";
+        chocolate.style.backgroundColor = "white";
+    }
+    else {
+        none.style.backgroundColor = "white";
+        sugar.style.backgroundColor = "white";
+        vanilla.style.backgroundColor = "white";
+        chocolate.style.backgroundColor = "#fce5cdff";
+    }
+}
